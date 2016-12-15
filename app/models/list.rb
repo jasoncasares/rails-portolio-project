d1f3b70be9bb52c4e74dc8955ddb6cf3d0593772
jasoncasares <jasonc@csdrafting.com>
2016-12-15 12:18:35 -0800

    add error for past due

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,6 +5,10 @@ class List < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :due_date, presence: true
 
-  scope :overdue, lambda { where('due_date: <= ?', Date.today) }
+  def self.overdue?
+    if due_date.past?
+      errors.add(:due_date, 'Past Due')
+    end
+  end
 
 end
