6f757d5adfd50a64a38a13529fd0115a731ed7c8
jasoncasares <jasonc@csdrafting.com>
2017-01-10 05:36:40 -0800

    clean up tasks.js

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -9,27 +9,19 @@ $(function(){
       success: function (task) {
       // get a response(variable data)
       var $ol = $("div.tasks ol")
-      $ol.html("") //emptied the ol
-      // iterate over each task within json
-      //console.log(response);
-        //response.forEach(function(task) {
-          // with each task data, append an li to the ol with the task content
-         $ol.append("<li>" + task.description + task.completed + "</li>");
-       //})
-
+        $ol.html("") //emptied the ol
+        // with each task data, append an li to the ol with the task content
+        $ol.append("<li>" + task.description + task.completed + "</li>");
       }
       //Load data into the DOM
       // load that response into the HTML of the page.
    })
    return e.preventDefault();
-})
+   })
 })
 
 $(function() {
   $("#new_task").submit(function(e) {
-    // need the URL to submit the POST request
-    // need the form data.
-
     $.ajax({
       type: "POST",
       url: this.action,
@@ -41,7 +33,6 @@ $(function() {
         var $ol = $("div.tasks ol")
         $ol.append("<li>" + response.description + "</li>");
       },
-      error: function(e) {console.log(e, "err")}
     });
     e.preventDefault();
   })
